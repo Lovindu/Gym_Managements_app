ef9b671db2be614456c7117125160cc9ed4b8b87
Lovindu Vihanga <lovinduvihanga93@gmail.com>
2023-06-26 14:23:09 +0530

    Created Finance Functions

--- Gym_Managements_app/User Controls/Finance.cs
+++ Gym_Managements_app/User Controls/Finance.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,52 @@ namespace Gym_Managements_app.User_Controls
         public Finance()
         {
             InitializeComponent();
+
+            ShowTransactionData();
+        }
+
+        private void ShowTransactionData()
+        {
+            string con = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\lovin\Documents\Gym_management_1 (1).mdf"";Integrated Security=True;Connect Timeout=30";
+
+            string qry = "select * from Transactions";
+
+            SqlDataAdapter da = new SqlDataAdapter(qry, con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Transactions");
+            dataGridPayments.DataSource = ds.Tables["Transactions"];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            EditSalary editSalary = new EditSalary();
+            editSalary.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            EditPackage editPackage = new EditPackage();
+            editPackage.Show();
         }
     }
 }
