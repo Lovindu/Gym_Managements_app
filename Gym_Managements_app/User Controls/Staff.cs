1dd583d0a49cf50da16abc69713606d0221b1bee
Tharindudaminthaovt <93636546+Tharindudaminthaovt@users.noreply.github.com>
2023-06-26 10:11:26 +0530

    payment search Transaction details added to database Add equipment show available equipment show current employees show available members Equipment Table Filled

--- Gym_Managements_app/User Controls/Staff.cs
+++ Gym_Managements_app/User Controls/Staff.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,11 +16,30 @@ namespace Gym_Managements_app.User_Controls
         public Staff()
         {
             InitializeComponent();
+            LoadStaffGrid();
         }
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void LoadStaffGrid()
+        {
+
+            string con = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\Tharindu\Downloads\Gym_management (1).mdf"";Integrated Security=True;Connect Timeout=30";
+            string qry = "select * from Staff";
+
+            SqlDataAdapter da = new SqlDataAdapter(qry, con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Staff");
+            dataGridView1.DataSource = ds.Tables["Staff"];
+
+            
         }
     }
 }
